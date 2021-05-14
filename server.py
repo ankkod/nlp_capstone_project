27d9c6a302d0830a73f7719e01cf56431aa90672
Ankit Kodalikar <ankitkodalikar94@gmail.com>
2021-05-14 17:02:08 +0530

    changes
    Signed-off-by: Ankit Kodalikar <ankitkodalikar94@gmail.com>

--- server.py
+++ server.py
@@ -45,6 +45,6 @@ def predict():
     #return jsonify(int(output))
     return render_template('result.html', prediction = prediction)
 
-
-# if __name__ == '__main__':
-#     app.run(port=8111, debug=True)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
