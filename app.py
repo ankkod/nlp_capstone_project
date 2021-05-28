c7cfd1cfa540ea5d4d616750a693b7890920ebc2
Ankit Kodalikar <ankitkodalikar94@gmail.com>
2021-05-28 23:43:02 +0530

    new model
    Signed-off-by: Ankit Kodalikar <ankitkodalikar94@gmail.com>

--- app.py
+++ app.py
@@ -2,12 +2,22 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
 from flask_cors import cross_origin
+import tensorflow as tf
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
 
 # Load the model
-model = pickle.load(open('model/NB_spam_model.pkl','rb'))
-vectorizer = CountVectorizer(decode_error="replace", vocabulary=pickle.load(open('model/feature.pkl','rb')))
+#model = pickle.load(open('model/NB_spam_model.pkl','rb'))
+#vectorizer = CountVectorizer(decode_error="replace", vocabulary=pickle.load(open('model/feature.pkl','rb')))
+# loading
+#with open('model/tokenizer.pl', 'rb') as handle:
+tokenizer = pickle.load(open('model/tokenizer.pl','rb'))
+
+model = tf.keras.models.load_model('model/bidir-lstm_model.h5')
 
 app = Flask(__name__)
 
@@ -23,17 +33,21 @@ def home():
 @app.route('/api',methods=['POST'])
 @cross_origin()
 def api_predict():
-    result = ['ham', 'spam']
     data = request.get_json(force=True)
     message = data['message']
     data = [message]
-    vect = vectorizer.transform(data).toarray()
-    prediction = model.predict(vect)
+    # vect = vectorizer.transform(data).toarray()
+    test_description = tokenizer.texts_to_sequences(data)
+    test_seq = pad_sequences(test_description)
+    print(test_seq)
+    print(model.predict(test_seq[0])[0].argmax())
+    prediction = model.predict(test_seq[0])[0].argmax()
     print(prediction)
     # Take the first value of prediction
-    output = prediction[0]
-    print(output)
-    return jsonify(result[output])
+    #output = prediction[0]
+    #print(output)
+    return jsonify(str(prediction))
+
 
 @app.route('/app',methods=['POST'])
 def predict():
@@ -41,12 +55,13 @@ def predict():
     # Make prediction using model loaded from disk as per the data.
     message = request.form['message']
     data = [message]
-    vect = vectorizer.transform(data).toarray()
-    prediction = model.predict(vect)
+    test_description = tokenizer.texts_to_sequences(data)
+    test_seq = pad_sequences(test_description)
+    prediction = model.predict(test_seq[0])[0].argmax()
     print(prediction)
     # Take the first value of prediction
-    output = prediction[0]
-    print(output)
+    #output = prediction[0]
+    #print(output)
     #return jsonify(int(output))
     return render_template('result.html', prediction = prediction)
 
